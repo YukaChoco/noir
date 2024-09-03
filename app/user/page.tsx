06f7d8963118903cde06f5d53661e0ac26d052eb
YukaChoco <sugar.ore.yk@gmail.com>
2024-09-03 16:32:29 +0900

    ユーザ画面の追加

---
+++ app/user/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.h1}>ユーザプロフィール</h1>
+    </main>
+  );
+}
