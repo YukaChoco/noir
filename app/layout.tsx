eaaf1d61861386e04b82fc5f3919c73c2d72a43b
YukaChoco <sugar.ore.yk@gmail.com>
2024-09-03 16:15:56 +0900

    meta titleのREAL修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "起きろ！朝ごはんREAL.",
+  title: "起きろ！朝ごはんReal.",
   description: "毎朝朝ごはんの写真を共有することで、互いに健康的な毎日をスタートすることができる新感覚SNSです。",
 };
 
