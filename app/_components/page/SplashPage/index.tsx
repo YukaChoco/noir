1f36ddb469845d480d444a53cb0d9344828f6fd8
YukaChoco <sugar.ore.yk@gmail.com>
2024-09-03 16:31:17 +0900

    splash pageの追加

---
+++ app/_components/page/SplashPage/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./page.module.css";
+
+const SplashPage = () => {
+  return (
+    <main className={styles.main}>
+      <h1>
+        起きろ！
+        <br />
+        朝ごはんReal
+      </h1>
+    </main>
+  );
+};
+
+export default SplashPage;
