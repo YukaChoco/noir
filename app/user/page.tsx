3109bdc01dd51061a9d1f633562b3f2ab5a36b49
marina <marina0724312@gmail.com>
2024-09-13 14:33:30 +0900

    make_user_page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -4,8 +4,14 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.h1}>ユーザプロフィール!!!!!
-        <p>アカウント名</p>
+      <h1 className={styles.h1}>ユーザプロフィール!
+        <div>アカウント名<img src="user_image.png"></img></div>
+        <div>べすと朝ごはん！
+          <img src="breakfast_image.png"></img>
+        </div>
+        <div>今週の朝ごはん <img src="breakfast_image.png"></img>
+          <div>○月○日</div></div>
+
       </h1>
     </main>
   );
